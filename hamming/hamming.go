b3d8b407286e43729b9d90d407bf83339eeda597
Briana Eng <brianaeng@gmail.com>
2017-02-28 20:57:51 -0800

    Hamming solution added

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,30 @@
 package hamming
 
+import (
+	"strings"
+	"errors"
+)
+
 const testVersion = 5
 
 func Distance(a, b string) (int, error) {
-	// your code here!
+	counter := 0
+
+	if len(a) == 0 || len(b) == 0 {
+		return 0, nil
+	} else if len(a) != len(b) {
+		err := errors.New("String too short")
+		return -1, err
+	}
+
+	stringA := strings.Split(a, "")
+	stringB := strings.Split(b, "")
+
+	for i := 0; i < len(a); i++ {
+		if stringA[i] != stringB[i] {
+			counter++
+		}
+	}
+
+	return counter, nil
 }
