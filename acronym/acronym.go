3263c12ae6c33df131d9cb4266ee1a16c2d259b7
Briana Eng <brianaeng@gmail.com>
2017-03-02 22:38:11 -0800

    Still working on acronym solution, need to loop through with IsUpper or something to catch uppercase letter that's not separated out.

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,5 +1,23 @@
 package acronym
 
+import (
+	"strings"
+)
+
 const testVersion = 2
 
-func Abbreviate(string) string
+func Abbreviate(longName string) string {
+	//if strings.Contains(longName, "-") {
+		alteredString := strings.Replace(longName, "-", " ", -1)
+	//}
+
+	splicedName := strings.Split(alteredString, " ")
+
+	abbreviation := ""
+
+	for _, v := range splicedName {
+		abbreviation += v[:1]
+	}
+
+	return strings.ToUpper(abbreviation)
+}
