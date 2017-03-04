54ba8763149bfc6022f1b9ec8609954419bb3be7
Briana Eng <brianaeng@gmail.com>
2017-03-04 11:17:10 -0800

    this one test is giving me so much grief that it has me in runes

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -2,14 +2,30 @@ package acronym
 
 import (
 	"strings"
+	//"unicode"
+	"fmt"
 )
 
 const testVersion = 2
 
 func Abbreviate(longName string) string {
 	//if strings.Contains(longName, "-") {
-		alteredString := strings.Replace(longName, "-", " ", -1)
+	alteredString := strings.Replace(longName, "-", " ", -1)
 	//}
+
+	altertedStringToRunes := []rune(alteredString)
+
+	fmt.Println(altertedStringToRunes)
+
+	// for i, v := range altertedStringToRunes[1:] {
+	// 	if unicode.IsUpper(v) {
+	// 		if !unicode.IsUpper(altertedStringToRunes[i-1]) {
+	// 			altertedStringToRunes = append(altertedStringToRunes, 0)
+	// 			copy(altertedStringToRunes[i-1:], altertedStringToRunes[i:])
+	// 			altertedStringToRunes[i] = ' '
+	// 		}
+	// 	}
+	// }
 
 	splicedName := strings.Split(alteredString, " ")
 
