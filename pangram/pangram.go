9258597569df3bdbc40f12549cca6b8049eba04c
Briana Eng <brianaeng@gmail.com>
2017-03-19 00:03:17 -0700

    Adding initial solution for pangram -- all tests pass

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,33 @@
 package pangram
 
+import (
+	"strings"
+)
+
 const testVersion = 1
 
-func IsPangram(string) bool
+func IsPangram(input string) bool {
+  if input == "" {
+    return false
+  }
+
+  letters := map[string]int{"a":0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0}
+
+  lowerString := strings.ToLower(input)
+
+  stringLetters := strings.Split(lowerString, "")
+
+  for _, letter := range stringLetters {
+    if _, ok := letters[letter]; ok {
+      letters[letter] += 1
+    }
+  }
+
+  for _, val := range letters {
+    if val == 0 {
+      return false
+    }
+  }
+
+  return true
+}
